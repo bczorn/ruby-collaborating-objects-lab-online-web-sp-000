57776b7d2d9534e0d3cdcac14b2dae5ef7e32314
Becky Zorn <bczorn@gmail.com>
2020-01-21 22:41:45 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,16 @@
 class MP3Importer
   
   attr_accessor :path
+  @@files = []
   
   def initialize(path)
     @path = path
   end
   
   def files
-    Dir.foreach(@path)
+    Dir.foreach(@path) do |x|
+      @@files << x
+    end
   end
   
   def import
